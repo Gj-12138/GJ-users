dc7122289bc4e0663c6ecf68516f53d997619ed1
Gj <2964820196@qq.com>
2024-11-23 23:57:01 +0800

    用户管理中心的后端 2024-11-23晚

--- src/main/java/com/gj/userbackend/UserBackendApplication.java
+++ src/main/java/com/gj/userbackend/UserBackendApplication.java
@@ -1,15 +1,24 @@
 package com.gj.userbackend;
 
+import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.DigestUtils;
 
 @SpringBootApplication
 @MapperScan("com.gj.userbackend.mapper")
 public class UserBackendApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(UserBackendApplication.class, args);
     }
+    @Test
+    void testDigest(){
+        String newPassword = DigestUtils.md5DigestAsHex(("abcd"+"mypassweord").getBytes());
+        System.out.println(newPassword);
+    }
+
+
+
 }
--- src/test/java/com/gj/userbackend/service/UserServiceTest.java
+++ src/test/java/com/gj/userbackend/service/UserServiceTest.java
@@ -1,6 +1,5 @@
 package com.gj.userbackend.service;
 
-import com.gj.userbackend.model.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,18 +12,51 @@ class UserServiceTest {
     @Resource
     private UserService userService;
 
+//    @Test
+//    void  testAddUser(){
+//        User user = new User();
+//        user.setUsername("gj");
+//        user.setUserAccount("123");
+//        user.setAvatarUrl("https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/logo.png");
+//        user.setGender(0);
+//        user.setUserPassword("xxx");
+//        user.setPhone("123");
+//        user.setEmail("123");
+//       boolean result = userService.save(user);
+//        System.out.println(user.getId());
+//        Assertions.assertTrue(result);
+//    }
+    /**
+     * 测试用户注册
+     */
     @Test
-    void  testAddUser(){
-        User user = new User();
-        user.setUsername("gj");
-        user.setUserAccount("123");
-        user.setAvatarUrl("https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/logo.png");
-        user.setGender(0);
-        user.setUserPassword("xxx");
-        user.setPhone("123");
-        user.setEmail("123");
-       boolean result = userService.save(user);
-        System.out.println(user.getId());
-        Assertions.assertTrue(result);
+    void userRegister() {
+        String userAccount = " ";
+        String userPassword = "";
+        String checkPassword = " ";
+        String planetCode = " ";
+        long result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yu";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yupi";
+        userPassword = "123456";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yu pi";
+        userPassword = "12345678";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        checkPassword = "123456789";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "dogYupi";
+        checkPassword = "12345678";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yupi";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
     }
 }
