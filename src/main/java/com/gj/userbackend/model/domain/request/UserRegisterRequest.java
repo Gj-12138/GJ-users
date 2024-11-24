a0062e7053399b7b3b09bc6bf11fb03b923f14f6
Gj <2964820196@qq.com>
2024-11-24 11:28:01 +0800

    用户管理中心的后端

---
+++ src/main/java/com/gj/userbackend/model/domain/request/UserRegisterRequest.java
@@ -0,0 +1,36 @@
+package com.gj.userbackend.model.domain.request;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户注册请求体
+ *
+ */
+@Data
+public class UserRegisterRequest implements Serializable {
+
+
+    private static final long serialVersionUID = 4682801063444415169L;
+    /**
+     * 用户账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户密码
+     */
+    private String userPassword;
+
+    /**
+     * 校验密码
+     */
+    private String checkPassword;
+
+    /**
+     * 星球编号
+     */
+    private String planetCode;
+
+}
